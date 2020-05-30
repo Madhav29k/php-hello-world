1a9350e323efa2eeb7acc259c56ca07d62cc687c
root <root@wisps1.fyre.ibm.com>
2020-05-30 07:47:42 -0700

    Suku version

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html>
 
 <head>
-<title>Hi Madhavan - Hello World!</title>
+<title>Hello World!</title>
 </head>
 
 <body>
 
-<?php echo "Hello World!"; ?>
+<?php echo "Hi- am sukumar - Hello World!"; ?>
 <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
 
 <?php $links = [];
